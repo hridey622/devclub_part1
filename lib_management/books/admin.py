8e7a973ca7df4e152dd40e73a7122247ff639cb4
hridey622 <67864127+hridey622@users.noreply.github.com>
2021-04-21 23:47:04 +0530

    'reviews'

---
+++ lib_management/books/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Book , Review
+
+
+class ReviewInline(admin.TabularInline):
+        model = Review
+
+
+class BookAdmin(admin.ModelAdmin):
+    inines = [
+        ReviewInline,
+    ]
+
+    list_display = ("title","author","publisher","genre","isbn","location","availability")
+
+
+
+
+admin.site.register(Book, BookAdmin)
---
+++ lib_management/books/models.py
@@ -0,0 +1,46 @@
+import uuid
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+class Book(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False    )
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    genre = models.CharField(max_length=10)
+    isbn = models.IntegerField()
+    location = models.CharField(max_length=1000)
+    availability = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('book_detail', args=[str(self.id)])
+
+
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+
+    def __str__(self):
+        return self.review
+
+
+
+
+
